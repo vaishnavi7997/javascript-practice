f56517edbc963ef5634f2267af726adbd8e125a3
vaishnavi7997 <Vaishnavi555222@gmail.com>
2025-02-08 10:30:36 +0530

    secound commit

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var employees = {
     salary: "1000000",
     age: 21
 }
-// console.log(employees);
-// console.log(typeof(employees));
-// console.log(typeof(salary));
-// console.log(employees["salary"]);
-// console.log(employees["age"]);
-// console.log(typeof(age));
+console.log(employees);
+console.log(typeof(employees));
+console.log(typeof(salary));
+console.log(employees["salary"]);
+console.log(employees["age"]);
+console.log(typeof(age));
 
 
 var  employees = ["sita" , "Rama", "lakshman"];
