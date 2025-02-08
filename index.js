2d18fa039f077b6a917d4f2df529ec2ccc51f5a6
vaishnavi7997 <Vaishnavi555222@gmail.com>
2025-02-08 09:57:37 +0530

    first javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+var employees = {
+    name: "vaishu",
+    salary: "1000000",
+    age: 21
+}
+// console.log(employees);
+// console.log(typeof(employees));
+// console.log(typeof(salary));
+// console.log(employees["salary"]);
+// console.log(employees["age"]);
+// console.log(typeof(age));
+
+
+var  employees = ["sita" , "Rama", "lakshman"];
+console.log(employees);
+console.log(typeof(employees));
